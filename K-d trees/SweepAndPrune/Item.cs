a0a226b6d1b006db89b656e445d1b82fc396d54a
DeyanDanailov <stormbringer_ss@abv.bg>
2021-01-08 12:57:31 +0200

    up

--- K-d trees/SweepAndPrune/Item.cs
+++ K-d trees/SweepAndPrune/Item.cs
@@ -1,10 +1,19 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿
 
 namespace SweepAndPrune
 {
-    class Class1
+    public class Item
     {
+        public Item(string id, int x1, int y1)
+        {
+            this.Id = id;
+            this.X1 = x1;
+            this.Y1 = y1;
+        }
+        public string Id { get; set; }
+        public int X1 { get; set; }
+        public int Y1 { get; set; }
+        public int X2 { get { return this.X1 + 10; } }
+        public int Y2 { get { return this.Y1 + 10; } }
     }
 }
--- K-d trees/SweepAndPrune/Program.cs
+++ K-d trees/SweepAndPrune/Program.cs
@@ -11,11 +11,11 @@ namespace SweepAndPrune
             var byId = new Dictionary<string, Item>();
 
             var running = true;
-
+            var ticks = 0;
             var line = Console.ReadLine();
             while (!line.Equals("end"))
             {
-                var cmdArgs = line.Split(' ');
+                var cmdArgs = line.Split();
                 switch (cmdArgs[0])
                 {
                     case "add":
@@ -28,13 +28,47 @@ namespace SweepAndPrune
                             {
                                 running = false;
                             }
+                            if (cmdArgs[0].Equals("move"))
+                            {
+                                var id = cmdArgs[1];
+                                var x = int.Parse(cmdArgs[2]);
+                                var y = int.Parse(cmdArgs[3]);
+                                byId[id].X1 = x;
+                                byId[id].Y1 = y;
+                            }
 
+                            Sweep(ticks, items);
                         }
                         break;
                     default:
                         break;
                 }
             }
+        }
+
+        private static void Sweep(int ticks, List<Item> items)
+        {
+            InsertionSort(items );
+        }
+
+        private static void InsertionSort(List<Item> items)
+        {
+            for (int i = 1; i < items.Count; i++)
+            {
+                var j = i;
+                while (j>0 && items[j -1].X1 > items[j].X1)
+                {
+                    Swap(j-1, j , items);
+                    j--;
+                }
+            }
+        }
+
+        private static void Swap(int v, int j, List<Item> items) // Swap places of items
+        {
+            var temp = items[v];
+            items[v] = items[j];
+            items[j] = temp;
         }
 
         private static void AddItem(List<Item> items, Dictionary<string, Item> byId, string[] cmdArgs)
