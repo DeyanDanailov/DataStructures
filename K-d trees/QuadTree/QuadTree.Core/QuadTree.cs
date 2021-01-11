c2789328fa3d09edba87d59eacee52f0c8db34fa
DeyanDanailov <stormbringer_ss@abv.bg>
2021-01-11 11:00:47 +0200

    up

--- K-d trees/QuadTree/QuadTree.Core/QuadTree.cs
+++ K-d trees/QuadTree/QuadTree.Core/QuadTree.cs
@@ -28,7 +28,55 @@ public class QuadTree<T> where T : IBoundable
 
     public bool Insert(T item)
     {
-        throw new NotImplementedException();
+        var current = this.root;
+        var depth = 1;
+        while (current.Children != null)
+        {
+            var q = GetQ(current, item.Bounds);
+            if (q == -1) break;
+    
+            current = current.Children[q];
+            depth++;
+        }
+        current.Items.Add(item);
+        TrySplit(current, depth);
+        this.Count++;
+        return true;
+    }
+
+    private void TrySplit(Node<T> node, int depth)
+    {
+        if (!node.ShouldSplit || depth >= MaxDepth)
+        {
+            return;
+        }
+        node.Children = new Node<T>[4];
+        var leftWidth = node.Bounds.Width / 2;
+        var rightWidth = node.Bounds.Width - leftWidth;
+        var topHeight = node.Bounds.Height / 2;
+        var botHeight = node.Bounds.Height - topHeight;
+
+        node.Children[0] = new Node<T>(node.Bounds.MidX, node.Bounds.Y1, rightWidth, topHeight);
+        node.Children[1] = new Node<T>(node.Bounds.X1, node.Bounds.Y1, leftWidth, topHeight);
+        node.Children[2] = new Node<T>(node.Bounds.X1, node.Bounds.MidY, leftWidth, botHeight);
+        node.Children[3] = new Node<T>(node.Bounds.MidX, node.Bounds.MidY, rightWidth, botHeight);
+
+        foreach (var item in node.Items)
+        {
+
+        }
+    }
+
+    private int GetQ(Node<T> current, Rectangle bounds)
+    {
+        if (current.Children != null)
+        {
+            if (bounds.IsInside(current.Children[0].Bounds)) return 0;
+            if (bounds.IsInside(current.Children[1].Bounds)) return 1;
+            if (bounds.IsInside(current.Children[2].Bounds)) return 2;
+            if (bounds.IsInside(current.Children[3].Bounds)) return 3;
+        }
+        return -1;
     }
 
     public List<T> Report(Rectangle bounds)
